b04720dba815f60ad1a34c8b936e2f691e86ed35
Nekodigi <uekaz@outlook.jp>
2024-05-26 13:59:32 +0900

    window fix

--- components/atoms/windowDimension.tsx
+++ components/atoms/windowDimension.tsx
@@ -1,19 +1,22 @@
+"use client";
+
 import { useState, useEffect } from "react";
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
-
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
 
   useEffect(() => {
+    function getWindowDimensions() {
+      const { innerWidth: width, innerHeight: height } = window;
+      return {
+        width,
+        height,
+      };
+    }
+
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
